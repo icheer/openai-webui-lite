789cd475a860f421b709e15b9820a86405f7cd7f
icheer <icheer@linux.do>
2025-11-03 16:32:01 +0800

    fix: tavily search

--- worker.js
+++ worker.js
@@ -369,6 +369,7 @@ async function handleRequest(request, env = {}) {
     // 接下来从modelResponse中提取content
     const modelJsonData = await modelResponse.json();
     console.log(modelJsonData);
+    console.log(JSON.stringify(modelJsonData));
     const content = modelJsonData.choices?.[0]?.message?.content || '';
     // 从中找到反引号`的位置, 提取反引号里包裹的内容
     // 从结果中找到花括号内容, 提取为JSON
