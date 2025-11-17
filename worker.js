eb2a88dbb6b9d3cf60d06cddb69d46b2be07996d
icheer <icheer@linux.do>
2025-11-17 11:15:02 +0800

    fix: getLiteModel

--- worker.js
+++ worker.js
@@ -805,6 +805,7 @@ function getLiteModelId(modelIds) {
     .map(i => i.split('=')[0].trim())
     .filter(i => i);
   const parts = [
+    'gcli/gemini-flash-latest',
     'gcli/gemini-2.5-flash',
     '-mini',
     '-nano',
